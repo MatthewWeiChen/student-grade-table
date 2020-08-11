e38ad1b267aa29a1d1ee4667a2f6b714987cdbf9
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-11 16:39:14 -0700

    completed user can view all grades

--- components/app.js
+++ components/app.js
@@ -1,5 +1,25 @@
 class App {
+  constructor(gradeTable) {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.gradeTable = gradeTable;
+  }
   handleGetGradesError(error) {
-    console.error(error);
+    console.error(error)
+  }
+  handleGetGradesSuccess(grades) {
+    this.gradeTable.updateGrades(grades);
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: { "X-Access-Token": "h0QMbjBH" },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
+  }
+  start() {
+    this.getGrades();
   }
 }
---
+++ main.js
@@ -0,0 +1,4 @@
+var tableSelect = document.querySelector('.table');
+var table = new GradeTable(tableSelect);
+var app = new App(table);
+app.start();
