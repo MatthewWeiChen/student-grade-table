f9fd0134e02f28abb5d95b723197fcb5723fe1cb
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-12 15:36:08 -0700

    added methods to app.js

--- components/app.js
+++ components/app.js
@@ -61,4 +61,22 @@ class App {
   handleCreateGradeSuccess() {
     this.getGrades();
   }
+
+  deleteGrade(id) {
+    console.log(id);
+  }
+
+  handleDeleteGradeError(error) {
+    console.error(error)
+  }
+
+  handleDeleteGradeSuccess() {
+    this.getGrades();
+  }
+
+
+
+
+
+
 }
