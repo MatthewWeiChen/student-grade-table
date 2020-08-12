718c1f2c12f8f74584255d8dfdcae51963872e20
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-12 12:08:28 -0700

    added gradeform.js and added submit event listener

---
+++ components/grade-form.js
@@ -0,0 +1,14 @@
+class Gradeform {
+  constructor(formElement) {
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener('submit', this.handleSubmit);
+  }
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event) {
+    preventDefault(event);
+    console.log('Hi');
+  }
+}
