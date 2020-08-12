49e1ac845bbd891a6a56aba0b45bcbc50b0bde99
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-11 17:31:24 -0700

    added second parameter pageHeader to constructor of App

--- components/app.js
+++ components/app.js
@@ -1,8 +1,9 @@
 class App {
-  constructor(gradeTable) {
+  constructor(gradeTable, pageHeader) {
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.gradeTable = gradeTable;
+    this.pageHeader = pageHeader;
   }
   handleGetGradesError(error) {
     console.error(error)
